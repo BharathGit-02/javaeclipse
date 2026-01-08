a427dd3f6e4404252d6b95083d109cc336056de6
manjum <manju.m.sree@gmail.com>
2026-01-08 13:04:28 +0530

    Commited friends code

--- githubsample/src/githubsample/GitHub.java
+++ githubsample/src/githubsample/GitHub.java
@@ -3,7 +3,7 @@ package githubsample;
 public class GitHub {
 	public static void main(String[] args) {
 		
-		System.out.println("HI,Good Morning.....");
+	
 		System.out.println("GitHub integration");
 		System.out.println("editing others code");
 		
