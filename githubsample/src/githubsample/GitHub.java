bf7c2aa05acfb3a2b468854fe55e2d53629283c7
manjum <manju.m.sree@gmail.com>
2026-01-08 11:56:55 +0530

    Code changed

--- githubsample/src/githubsample/GitHub.java
+++ githubsample/src/githubsample/GitHub.java
@@ -2,6 +2,10 @@ package githubsample;
 
 public class GitHub {
 	public static void main(String[] args) {
+		
+		System.out.println("HI,Good Morning.....");
 		System.out.println("GitHub integration");
+		System.out.println("editing others code");
+		
 	}
 }
